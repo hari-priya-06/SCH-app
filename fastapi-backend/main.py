e1e1fe53a98483cc349cea6c32baa76ee90f5c99
hari-priya-06 <haripriyamalladi06@gmail.com>
2025-07-10 23:03:53 +0530

    test

--- fastapi-backend/main.py
+++ fastapi-backend/main.py
@@ -23,11 +23,9 @@ cloudinary.config(
 app = FastAPI()
 
 origins = [
-    "http://localhost:5173",
-    "http://localhost:5174",
     "http://localhost:5175",
     "http://localhost:3000",
-    "https://extraordinary-bunny-c449c6.netlify.app",
+    "https://extraordinary-bunny-c449c6.netlify.app",  # ✅ Good!
 ]
 app.add_middleware(
     CORSMiddleware,
